972e23e1ef869afc8a60a42e2fef78fb114ed34a
huhongwei <381093478@qq.com>
2024-11-19 15:41:02 +0800

    fix

--- apps/app/src/utils/misc.ts
+++ apps/app/src/utils/misc.ts
@@ -4,8 +4,5 @@ import { CURRENCIES } from "@v1/backend/convex/schema";
  * Locales.
  */
 export function getLocaleCurrency() {
-  return navigator.languages.includes("en-US")
-    ? CURRENCIES.USD
-    : // Only support USD for now due to Polar limitations.
-      CURRENCIES.USD;
+  return CURRENCIES.USD;
 }
